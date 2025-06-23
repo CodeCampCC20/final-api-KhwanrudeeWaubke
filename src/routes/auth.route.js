980dfbd3ff2309054527700b43a372b021a7ebc0
khwanrudeewaubke <nan_khwanrudee@hotmail.com>
2025-06-23 18:00:21 +0700

    i

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authController from "../controllers/auth.controller.js";
+import validatorMiddleware from "../middlewares/validate.middleware.js";
+import { schemaRegister } from "../utils/schema.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register/doctor", authController.registerDoctor);
+authRouter.post("/register/doctor", validatorMiddleware(schemaRegister), authController.registerDoctor);
 authRouter.post("/login/doctor", authController.doctorLogin);
 
 authRouter.post("/register/user", authController.registerUser);
